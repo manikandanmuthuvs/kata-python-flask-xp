2b4e6b18158f3c1409d7e9425697cd4d852ebcc1
Manikandan Muthuvs <manikandan.muthuvs@gmail.com>
2019-09-06 15:34:09 +0530

    added dockerhub config in travis file

--- test_app.py
+++ test_app.py
@@ -3,7 +3,7 @@ import unittest
 class SimpleTest(unittest.TestCase): 
   
     # Returns True or False.  
-    def test(self):         
+    def testAssert(self):         
         self.assertTrue(True) 
   
 if __name__ == '__main__': 
