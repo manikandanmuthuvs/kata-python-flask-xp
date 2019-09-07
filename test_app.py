f79ff730ff46b9cade241c0a88bfa893dbd69a0a
Manikandan Muthuvs <manikandan.muthuvs@gmail.com>
2019-09-07 10:58:51 +0530

    updated dockerfile to use python image

--- test_app.py
+++ test_app.py
@@ -3,7 +3,7 @@ import unittest
 class SimpleTest(unittest.TestCase): 
   
     # Returns True or False.  
-    def testAssert(self):         
+    def test(self):         
         self.assertTrue(True) 
   
 if __name__ == '__main__': 
