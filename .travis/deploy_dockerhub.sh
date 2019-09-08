4c68f194d18d64d54707b9e127ee3e7c61528da1
Manikandan Muthuvs <manikandan.muthuvs@gmail.com>
2019-09-08 07:47:37 +0530

    updated dockerfile to use python image

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,9 +1,10 @@
-docker login --username $DOCKER_USER --password $DOCKER_PASS
+# docker login --username $DOCKER_USER --password $DOCKER_PASS
+docker login -u "manikandanmuthuv" -p "N@ukr!123" docker.io
 if [ "$TRAVIS_BRANCH" = "master" ]; then
 TAG="latest"
 else
 TAG="$TRAVIS_BRANCH"
 fi
 docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
-docker tag $TRAVIS_REPO_SLUG $DOCKER_USER/$TRAVIS_REPO_SLUG
-docker push $DOCKER_USER/$TRAVIS_REPO_SLUG
+docker tag $TRAVIS_REPO_SLUG $DOCKER_USER/kata-python-flask-xp:sample
+docker push $DOCKER_USER/kata-python-flask-xp:sample
